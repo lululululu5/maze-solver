46628f119cc7538b855d91ee3410ab85d756489e
Lukas <rsslks@gmail.com>
2024-06-09 12:39:28 +0200

    Altered the input parameters of the maze

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from maze import Maze
 
 def main():
     win = Window(800, 600)
-    maze = Maze(0, 0, 25, 25, 25, 25, win, 12)
+    maze = Maze(0, 0, 25, 25, 25, 25, win)
     maze.solve()
     win.wait_for_close()
 
